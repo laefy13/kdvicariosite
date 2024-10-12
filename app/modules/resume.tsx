83cf956eed57fa7322795ce8e43df935bac99183
laefy13 <karloosss13@gmail.com>
2024-10-12 15:37:47 +0900

    updated resume

--- app/modules/resume.tsx
+++ app/modules/resume.tsx
@@ -9,108 +9,101 @@ export default function Resume() {
       <div className="resume-main">
         <div className="white-space mb-1" />
         <div className="white-spacee" id="resume" />
-        <div className="info-main">
-          <div className="info-container">
-            <div className="resume-title">
-              <div className="floating-square" />
-              <div className="mb-10 mt-20 text-center text-5xl font-extrabold leading-none tracking-tight text-gray-900 sm:text-3xl  md:text-5xl lg:text-5xl  dark:text-white">
-                RESUME
-              </div>
-            </div>
-            <div className="info-title">
-              <div
-                id="workexperience"
-                className="mb-7 text-4xl font-extrabold leading-none tracking-tight text-gray-900 sm:text-3xl  md:text-4xl lg:text-4xl  dark:text-white"
-              >
-                Work Experience
-              </div>
-            </div>
-            <Info>
-              <div className=" mb-1 text-3xl font-extrabold leading-none tracking-tight text-gray-900 sm:text-2xl  md:text-3xl lg:text-3xl  dark:text-white">
-                DOST Project LODI
-              </div>
 
-              <Title
-                title="Intern Developer"
-                size={2}
-                mb={1}
-                color="black"
-                additional="break-words"
-              />
-              <li>
-                Developing modules and web pages with React JS and API routes
-                with Laravel
-              </li>
-              <li>Debugging assigned modules and web pages</li>
-              <li>
-                Debugging coworker&apos;s webpages and modules, and finishing
-                unfinished modules
-              </li>
-            </Info>
-            <Info>
-              <Title
-                title="SKYWORKSTECH SOFTWARE DEVELOPMENT SERVICES"
-                size={3}
-                mb={1}
-                color="white"
-                additional="break-words"
-              />
-
-              <Title
-                title="Freelance Developer"
-                size={2}
-                mb={1}
-                color="black"
-              />
-              <li>
-                Developing client&apos;s request with PHP, JavaScript, HTML and
-                CSS
-              </li>
-            </Info>
+        <div className="resume-title">
+          <div className="floating-square" />
+          <div className="mb-10 mt-20 text-center text-5xl font-extrabold leading-none tracking-tight text-gray-900 sm:text-3xl  md:text-5xl lg:text-5xl  dark:text-white">
+            RESUME
           </div>
         </div>
         <div className="info-container">
           <div className="info-title">
             <Title
-              title="Certifications"
+              title="Education"
               size={4}
               mb={7}
               additional="text-left"
               color="white"
-              id="certifications"
+              id="education"
             />
           </div>
           <Info>
             <Title
-              title="DOST, Project League of Developers Initiative (LODI) Certificate of Completion"
-              size={1}
-              mb={2}
-              color="black"
+              title="Dasmarinas Integrated High School"
+              size={3}
+              mb={1}
+              color="white"
             />
+
+            <div>
+              <Title title="Junior High School" size={2} mb={1} color="black" />
+              2014-2018
+            </div>
           </Info>
           <Info>
             <Title
-              title="DICT-LMS, Programming for Intermediate Users Using Python Certificate of Completion"
-              size={1}
-              mb={2}
-              color="black"
+              title="Emilio Aguinaldo College Cavite"
+              size={3}
+              mb={1}
+              color="white"
             />
+
+            <div>
+              <Title title="Senior High School" size={2} mb={1} color="black" />
+              2018-2020
+            </div>
           </Info>
           <Info>
             <Title
-              title="DICT-LMS, Advanced Level of Cloud Computing Certificate of Completion"
-              size={1}
-              mb={2}
-              color="black"
+              title="Polytechnic University of the Philippines"
+              size={3}
+              mb={1}
+              color="white"
             />
+
+            <div>
+              <Title
+                title="BS Computer Science"
+                size={2}
+                mb={1}
+                color="black"
+              />
+              2020-2024
+              <li>Cum Laude</li>
+              <li>GPA Average of 1.376</li>
+            </div>
           </Info>
+        </div>
+        <div className="info-container">
+          <div className="info-title">
+            <div
+              id="workexperience"
+              className="mb-7 text-4xl font-extrabold leading-none tracking-tight text-gray-900 sm:text-3xl  md:text-4xl lg:text-4xl  dark:text-white"
+            >
+              Work Experience
+            </div>
+          </div>
           <Info>
+            <div className=" mb-1 text-3xl font-extrabold leading-none tracking-tight text-gray-900 sm:text-2xl  md:text-3xl lg:text-3xl  dark:text-white">
+              DOST Project LODI
+            </div>
+
             <Title
-              title="DICT-LMS, Advanced Level of Software Engineering Certificate of Completion"
-              size={1}
-              mb={2}
+              title="Intern Developer"
+              size={2}
+              mb={1}
               color="black"
+              additional="break-words"
             />
+            <li>
+              Developing modules and web pages with React JS and API routes with
+              Laravel
+            </li>
+            <li>Debugging assigned modules and web pages</li>
+            <li>
+              Debugging coworker&apos;s webpages and modules, and finishing
+              unfinished modules
+            </li>
           </Info>
         </div>
         <div className="info-container">
@@ -190,6 +183,7 @@ export default function Resume() {
             </p>
           </Info>
         </div>
+
         <div className="info-container">
           <div className="info-title">
             <Title
@@ -231,65 +225,51 @@ export default function Resume() {
             </p>
           </Info>
         </div>
+
         <div className="info-container">
           <div className="info-title">
             <Title
-              title="Education"
+              title="Certifications"
               size={4}
               mb={7}
               additional="text-left"
               color="white"
-              id="education"
+              id="certifications"
             />
           </div>
           <Info>
             <Title
-              title="Dasmarinas Integrated High School"
-              size={3}
-              mb={1}
-              color="white"
+              title="DOST, Project League of Developers Initiative (LODI) Certificate of Completion"
+              size={1}
+              mb={2}
+              color="black"
             />
-
-            <div>
-              <Title title="Junior High School" size={2} mb={1} color="black" />
-              2014-2018
-            </div>
           </Info>
           <Info>
             <Title
-              title="Emilio Aguinaldo College Cavite"
-              size={3}
-              mb={1}
-              color="white"
+              title="DICT-LMS, Programming for Intermediate Users Using Python Certificate of Completion"
+              size={1}
+              mb={2}
+              color="black"
             />
-
-            <div>
-              <Title title="Senior High School" size={2} mb={1} color="black" />
-              2018-2020
-            </div>
           </Info>
           <Info>
             <Title
-              title="Polytechnic University of the Philippines"
-              size={3}
-              mb={1}
-              color="white"
+              title="DICT-LMS, Advanced Level of Cloud Computing Certificate of Completion"
+              size={1}
+              mb={2}
+              color="black"
             />
-
-            <div>
-              <Title
-                title="BS Computer Science"
-                size={2}
-                mb={1}
-                color="black"
-              />
-              2020-2024
-              <li>Cum Laude</li>
-              <li>GPA Average of 1.376</li>
-            </div>
+          </Info>
+          <Info>
+            <Title
+              title="DICT-LMS, Advanced Level of Software Engineering Certificate of Completion"
+              size={1}
+              mb={2}
+              color="black"
+            />
           </Info>
         </div>
-
         <div className="info-container">
           <div className="info-title">
             <Title
